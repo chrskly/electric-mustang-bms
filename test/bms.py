b15ba9fa9d3356c11b9bd9499776f4f50ef2b79a
Christian Kelly <chrskly@chrskly.com>
2024-02-29 19:59:54 +0000

    Split out from other repo

---
+++ test/bms.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+class bms:
+
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
---
+++ test/charger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+""" Simlate the CHAdeMO/CCS charger """
+
+class Charger:
+
+    def __init__(self):
+        pass
+
+    def start_charging(self):
+        pass
+
+    def stop_charging(self):
+        pass
+
